4201d1b8de6546a895ed7025eecd753128faba86
neul0129 <neul0129@naver.com>
2022-01-20 01:34:26 +0900

    first
    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './components/shared/Layout';
+import youtubeData from './data/youtubeData.json';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		
+  console.log('id : ', youtubeData['data'][0]['id'], ' -> 영상의 링크 주소');
+  console.log('channelId : ', youtubeData['data'][0]['channelId'], ' -> 채널 아이디');
+  console.log('date : ', youtubeData['data'][0]['date'], ' -> 업로드 날짜');
+  console.log('title : ', youtubeData['data'][0]['title'], ' -> 영상제목');
+  console.log('thumbnail : ', youtubeData['data'][0]['thumbnail'], ' -> 영상 썸네일 이미지 주소');
+  console.log('description : ', youtubeData['data'][0]['description'], ' -> 영상 설명');
+  console.log('channelTitle : ', youtubeData['data'][0]['channelTitle'], ' -> 채널 제목');
+	console.log('category : ', youtubeData['data'][0]['category'], ' -> 카테고리');
+  console.log('viewCount : ', youtubeData['data'][0]['viewCount'], ' -> 조회수');
+  console.log('likeCount : ', youtubeData['data'][0]['likeCount'], ' -> 좋아요 수');
+  console.log('channelUrl : ', youtubeData['data'][0]['channelUrl'], ' -> 채널 주소');
+  console.log('channelThumbnail : ', youtubeData['data'][0]['channelThumbnail'], ' -> 채널 썸네일 이미지 주소');
+
+
+	return(
+		<Layout>
+			<div>콘텐츠!</div>
+		</Layout> 
+		
+	);
 }
 
 export default App;
---
+++ src/components/shared/Header.js
@@ -0,0 +1,14 @@
+import styles from './Header.module.css';
+import { IconName } from "react-icons/bi";
+import {FiArrowDown} from 'react-icons';
+
+function Header() {
+  return(
+	<div>
+		<FiArrowDown />
+		헤더!
+	</div>
+		);
+}
+
+export default Header;
