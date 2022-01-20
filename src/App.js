82b403a440dc160bcebdfc9eee28a07adee4b6ca
neul0129 <neul0129@naver.com>
2022-01-20 17:50:32 +0900

    9일차
    9일차

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import './App.css';
-import Layout from './components/shared/Layout';
-import youtubeData from './data/youtubeData.json';
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Explore from './pages/Explore';
+import Subscription from './pages/Subscription';
 
 function App() {
-		
-  console.log('id : ', youtubeData['data'][0]['id'], ' -> 영상의 링크 주소');
-  console.log('channelId : ', youtubeData['data'][0]['channelId'], ' -> 채널 아이디');
-  console.log('date : ', youtubeData['data'][0]['date'], ' -> 업로드 날짜');
-  console.log('title : ', youtubeData['data'][0]['title'], ' -> 영상제목');
-  console.log('thumbnail : ', youtubeData['data'][0]['thumbnail'], ' -> 영상 썸네일 이미지 주소');
-  console.log('description : ', youtubeData['data'][0]['description'], ' -> 영상 설명');
-  console.log('channelTitle : ', youtubeData['data'][0]['channelTitle'], ' -> 채널 제목');
-	console.log('category : ', youtubeData['data'][0]['category'], ' -> 카테고리');
-  console.log('viewCount : ', youtubeData['data'][0]['viewCount'], ' -> 조회수');
-  console.log('likeCount : ', youtubeData['data'][0]['likeCount'], ' -> 좋아요 수');
-  console.log('channelUrl : ', youtubeData['data'][0]['channelUrl'], ' -> 채널 주소');
-  console.log('channelThumbnail : ', youtubeData['data'][0]['channelThumbnail'], ' -> 채널 썸네일 이미지 주소');
-
-
-	return(
-		<Layout>
-			<div>콘텐츠!</div>
-		</Layout> 
-		
-	);
-}
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/subscription" element={<Subscription />} />
+        </Routes>
+    );
+	}
 
 export default App;
